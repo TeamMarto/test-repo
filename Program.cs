489af34babad14e1c523a21448e43c5465dbc865
Martin <marrtingeorgiev@abv.bg>
2017-05-30 00:06:54 +0300

    upload the source code // Program.cs

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTestingClass
+{
+    class Program
+    {
+
+       
+
+
+
+        static void Main(string[] args)
+        {
+            // Hello my dear friends : Iwan , Kala and Slav
+            // 
+
+
+
+            Console.WriteLine();
+            for (int i = 1;i<=100;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Hello");
+                }
+
+                else
+                    Console.WriteLine("Iwan,Kala and Slav");
+
+            }
+
+            string exit = Console.ReadLine();
+
+
+        }
+    }
+}
--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
+program .cs - source code
 mojete da si drupnete samo .exe faila ot for
 i shte vi izpulni programata.
 Kompa sig shte vi dade che ne e sig iztochnika :D
